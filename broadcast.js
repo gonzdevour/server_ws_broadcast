f0888271a9d5a87e49ac3ab6f17d63c95ed3ca51
gonzdevour <jyunru.li@gmail.com>
2017-05-22 15:58:57 +0800

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocketServer({server:server});
 
 // Broadcast to all.
 wss.broadcast = function broadcast(data) {
